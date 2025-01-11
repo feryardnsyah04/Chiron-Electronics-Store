8b11ba15749713f316281a0c718a8773aac53aee
welres <dmspradita@gmail.com>
2025-01-11 18:15:13 +0700

    add contact us section, update about page content, and improve shop product details

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -6,6 +6,7 @@
 <div class="container py-5">
     <div class="header">
         <h1><b>Kontak Kami</b></h1>
+        <hr>
     </div>
 
     <div class="contact-details">
@@ -20,8 +21,9 @@
 
         <div class="map">
             <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.519008103285!2d106.81795051903427!3d-6.195041115387274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5849ccc0371%3A0xb04ce66a7cb711d9!2sGrand%20Indonesia!5e0!3m2!1sid!2sid!4v1735994525349!5m2!1sid!2sid"
-                allowfullscreen>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.517452672106!2d106.81511128885496!3d-6.195248099999982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5849ccc0371%3A0xb04ce66a7cb711d9!2sGrand%20Indonesia!5e0!3m2!1sid!2sid!4v1736544864936!5m2!1sid!2sid"
+            allowfullscreen=""                
+                loading="lazy">
             </iframe>
         </div>
     </div>
